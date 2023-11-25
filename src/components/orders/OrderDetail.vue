97366fcc428404b27674cd7cac72e09c4e87611f
MrBrenlla <bgbrenlla@gmail.com>
2023-11-25 20:48:22 +0100

    Listo OrderList e OrderDetail xunto con algo de css para formatear

--- src/components/orders/OrderDetail.vue
+++ src/components/orders/OrderDetail.vue
@@ -1,7 +1,79 @@
 <template>
   <v-container>
-    <h1 class="text-center">Order detail</h1>
+    <h1 class="text-center"  :style="{ fontSize: '50px' }">Order {{ $route.params.id }}</h1>
+	<ul>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+
+                <th>Reference</th>
+				<th>Date</th>
+				<th>Price</th>
+				<th>State</th>
+
+            </tr>
+            </thead>
+            <tbody>
+			<tr>
+ 
+                <td>{{ this.order.reference }}</td>
+				<td>{{ this.order.date }}</td>
+				<td>{{ this.order.price }}</td>
+				<td>{{ this.order.state }}</td>
+                
+			</tr>
+            </tbody>
+        </table>
+      </div>
+    </ul>
+	<hr>
+	<br>
+	<h1 class="text-center"  :style="{ fontSize: '25px' }">Items</h1>
+	<ul>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+
+                <th>Reference</th>
+				<th>Quantity</th>
+
+            </tr>
+            </thead>
+            <tbody>
+			<tr v-for="(item) in order.items" :key="item.itemReference">
+ 
+                <td>{{ item.itemReference }}</td>
+				<td>{{ item.numberItems }}</td>
+
+                
+			</tr>
+            </tbody>
+        </table>
+      </div>
+    </ul>
   </v-container>
 </template>
-<script setup>
+<script>
+import OrdersRepository from "@/repositories/OrdersRepository";
+
+export default {
+  data() {
+    return { 
+      order: []
+    }
+  },
+  created() {
+    this.getOrder(this.$route.params.id);
+  },
+  methods: {
+    getOrder(id) {
+      OrdersRepository.get(id)
+          .then((response) => {
+            this.order = response.data;
+          });
+    },
+  }
+}
 </script>
--- src/components/orders/OrderList.vue
+++ src/components/orders/OrderList.vue
@@ -1,6 +1,35 @@
 <template>
   <v-container>
-    <h1 class="text-center">Orders List</h1>
+    <h1 class="text-center"  :style="{ fontSize: '50px' }">Orders List</h1>
+	<ul>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+
+                <th>Reference</th>
+				<th>Date</th>
+				<th>Price</th>
+				<th>State</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="trSelect" v-for="(order) in orders" :key="order._id" @click="handleClick(order.reference)" >
+ 
+                <td >{{ order.reference }}</td>
+				<td>{{ order.date }}</td>
+				<td>{{ order.price }}</td>
+				<td>{{ order.state }}</td>
+                
+              </tr>
+            </tbody>
+        </table>
+      </div>
+    </ul>
+	<div class="flexbox-align">
+		<a class="btn lightButton text-xl" href="orders/new"> + </a>
+	</div>
   </v-container>
 </template>
 <script>
@@ -8,7 +37,7 @@ import OrdersRepository from "@/repositories/OrdersRepository";
 
 export default {
   data() {
-    return {
+    return { 
       orders: []
     }
   },
@@ -19,9 +48,12 @@ export default {
     getOrders() {
       OrdersRepository.getAll()
           .then((response) => {
-            this.orders = response;
+            this.orders = response.data;
           });
-    }
+    },
+	handleClick(reference) {
+      window.location.href = `orders/${reference}`;
+    },
   }
 }
 </script>
