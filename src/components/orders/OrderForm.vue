ed9f69d8c9e0a576a3d97ff3f3a1bf2353717eec
Pablo Mariño Boga <pablo.marino.boga@gmail.com>
2023-11-27 16:17:54 +0100

    Adds duplicates combine, and further form verification

--- src/components/orders/OrderForm.vue
+++ src/components/orders/OrderForm.vue
@@ -1,33 +1,30 @@
-// TODO: Add validation to the form 
-// TODO: Load the order data if in edit mode
-// TODO: remove Items with 0 quantity
-// FIXME: Error saving ITEMS
+// FIXME: Update order not working
 <template>
   <v-container>
-    <h1 class="text-center text-5xl p-10">Order Form</h1>
+    <h1 class="text-center text-5xl p-10">Order {{ this.$route.params.id  }} Form</h1>
     <div class="flex items-center justify-center pt-5">
       <div class="bg-base-200 shadow-xl p-10">
         <form @submit.prevent="submitForm">
           <p class="text-2xl pb-4">Order:</p>
           <div class="flex justify-between pb-2">
             <label class="mr-10" for="reference">Reference:</label>
-            <input class="w-30 text-right" type="number" min="0" v-model="reference" required />
+            <input class="w-30 text-right" type="number" min="0" v-model="reference" required  :disabled="isEditMode"/>
           </div>
 
           <div class="flex justify-between pb-2">
             <label class="mr-10" for="date">Date:</label>
-            <input class="w-100" type="date" v-model="date" required />
+            <input class="w-100" type="date" v-model="date" required :disabled="isEditMode"/>
           </div>
 
           <div class="flex justify-between pb-2">
             <label class="mr-10" for="state">State:</label>
-            <input class="w-30 text-right" disabled type="text" v-model="state" readonly />
+            <input class="w-30 text-right" type="text" v-model="state" readonly disabled/>
           </div>
 
-          <p class="text-2xl pt-4 pb-4">Items:</p>
+          <p class="text-2xl pt-4 pb-2">Items:</p>
 
-          <ul class="table-zebra">
-            <li class="flex justify-between pb-4" v-for="(item, index) in items" :key="index">
+          <ul>
+            <li class="flex justify-between p-2 mb-2 bg-base-100 rounded" v-for="(item, index) in items" :key="index">
               <div>
                 <label class="mr-4" for="itemReference">Item Reference:</label>
                 <input class="w-10 mr-6" type="number" min="0" v-model="item.itemReference" required />
@@ -38,7 +35,7 @@
                 <input class="w-10 mr-6" type="number" min="0" v-model="item.numberItems" required />
               </div>
 
-              <button class="btn btn-xs btn-error" @click="removeItem(index)">
+              <button class="btn btn-xs btn-error" type="button" @click="removeItem(index)">
                 Remove Item
               </button>
             </li>
@@ -53,6 +50,7 @@
           </ul>
 
           <div class="flex justify-between">
+            
             <button class="btn btn-primary mt-10" type="button" @click="backToOrders()">
               Cancel
             </button>
@@ -68,88 +66,132 @@
 import OrdersRepository from "@/repositories/OrdersRepository";
 
 export default {
+
   data() {
     return {
       id: "",
       reference: 0,
       date: "",
       state: "PENDING",
-      itemReference: "0",
+      itemReference: 0,
       numberItems: 0,
       items: [],
-      isEditMode: null, // Flag to determine if in edit mode4
+      isEditMode: this.$route.params.id != undefined,
     };
-  },
-  
-  beforeRouteEnter(to, from, next) {
-    // Determine if the route is for editing or creating a new order
-    const isEditMode = to.name === "Edit Order Form" && to.params.id !== undefined;
-    
-    next((vm) => {
-      // Pass the isEditMode flag to the component instance
-      vm.isEditMode = isEditMode;
-      console.log("isEditMode_before_route: " + isEditMode+" "+vm.isEditMode);
-    });
   },
 
   created() {
-    console.log("isEditMode_created: " + this.isEditMode);
     if (this.isEditMode) {
-      this.getOrder();
+      OrdersRepository.get(this.$route.params.id)
+        .then((response) => {
+          this.id = response.data.id;
+          this.reference = response.data.reference;
+          const [day, month, year] = response.data.date.split("-");
+          const date = new Date(year, month - 1, day);
+          this.date = date.toISOString().substr(0, 10);
+          this.state = response.data.state;
+          this.items = response.data.items;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     } else {
-      this.id = "";
       this.date = new Date().toISOString().substr(0, 10);
-      this.items = [{ itemReference: "0", numberItems: 0 }];
+      this.items = [{ itemReference: 0, numberItems: 0 }];
     }
   },
-  
+
   methods: {
     addItem() {
-      this.items.push({ itemReference: "0", numberItems: 0 });
+      this.items.push({ itemReference: 0, numberItems: 0 });
     },
     removeItem(index) {
-      if (this.items.length > 1) {
-        this.items.splice(index, 1);
+      if (this.items.length <= 1) {
+        alert("There must be at least one item in the order.");
+        return;
       }
-    }, backToOrders() {
+      this.items.splice(index, 1);
+    }, 
+    backToOrders() {
       this.$router.push("/orders/");
     },
-  }, async getOrder() {
-    OrdersRepository.getOne(this.$route.params.id)
-      .then((response) => {
-        this.id = response.data.id;
-        this.reference = response.data.reference;
-        this.date = response.data.date;
-        this.state = response.data.state;
-        this.items = response.data.items;
-        console.log("---------------" + response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-  async submitForm() {
-    const entity = JSON.stringify({
-      id: this.id,
-      reference: this.reference,
-      date: this.date,
-      state: "DONE", //this.state,
-      items: this.items,//JSON.stringify(this.items).split("\\").join(""), //
-    }).split("\\").join("");
-    console.log("Form data:", entity);
+    async submitForm() {
+      //Check duplicate items and combine them
+      this.combineItems();
 
-    try {
-      const result = await OrdersRepository.save(entity);
-      // If save is successful, do something with the result
-      alert("Order #" + this.reference + " saved successfully. ");
-      console.log("Save successful:", result);
-      this.backToOrders();
-      // Call another method or update the state as needed
-    } catch (error) {
-      // If there's an error, alert it
-      console.error("Save failed:", error);
-      alert("Save failed. Please try again.");
-    }
+      // Validate form
+      if (!this.isValidReference()) {
+        alert("Please check the form. Reference must be greater than 0.");
+        return;
+      }
+
+      if (!this.isValidItems()) {
+        alert("Please check the items. For each item, item reference and number of items must be greater than 0.");
+        return;
+      }
+
+      // convert reference to string
+      this.reference=String(this.reference);
+
+      // convert itemReference to string
+      this.items.forEach(item => {
+        item.itemReference = String(item.itemReference);
+      });
+
+      // Setup entity
+      const entity = 
+      {
+        //id: this.id,
+        reference: this.reference,
+        date: this.date,
+        state: "DONE", 
+        items: this.items,
+      };
+      
+      // Submit
+      try {
+        const result = await OrdersRepository.save(entity);
+        // Save is successful
+        alert("Order #" + this.reference + " saved successfully. ");
+        console.log("Save successful:", result);
+        this.backToOrders();
+      } catch (error) {
+        // Save error
+        alert("Save failed. Please try again.");
+        console.error("Save failed:", error);
+      }
+    },
+    isValidReference(){
+      return this.reference > 0;
+    },
+    isValidItems(){
+      for (let i = 0; i < this.items.length; i++) {
+        if (this.items[i].itemReference == 0 || this.items[i].numberItems == 0) {
+          return false;
+        }
+      }
+      return true;
+    },
+
+    combineItems(){
+      let combined = false;
+      let tmpItems = {}
+      this.items.forEach((item) => {
+        if(tmpItems[item.itemReference]){
+          tmpItems[item.itemReference] += item.numberItems
+          combined = true;
+        }else{
+          tmpItems[item.itemReference] = item.numberItems
+        } 
+        this.items = [];
+        for (let i in tmpItems){
+          this.items.push({itemReference: i, numberItems: tmpItems[i]});
+        }
+      });
+      if(combined){
+        alert("Duplicate items found. They have been combined.");
+      }
+    },
   },
 };
 </script>
