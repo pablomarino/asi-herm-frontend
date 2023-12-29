8a9fde300a15bd3ac50358e045f92f02484bc3c9
Xaime Pardal <xaime_pardal@outlook.com>
2023-12-29 17:02:53 +0100

    update init.sh

--- init-mongo.sh
+++ init-mongo.sh
@@ -14,30 +14,52 @@ send_request() {
 }
 
 send_request "POST" "items" '{
-  "name": "pieza 01",
+  "name": "pieza 4b",
   "reference": "1",
-  "description": "pieza de prueba",
+  "description": "pieza de tractor moncho SL",
   "purchasePrice": 50.20,
   "salePrice": 120
+}'
+
+send_request "POST" "items" '{
+  "name": "pieza 23f",
+  "reference": "2",
+  "description": "pieza de pascuali modelo 4578H",
+  "purchasePrice": 200.89,
+  "salePrice": 400
+}'
+
+send_request "POST" "boxes" '{
+  "reference": "1",
+  "size": "120x120x5",
+  "location": "E20-F05",
+  "itemReference": "1",
+  "numItems": 25
 }'
 
 send_request "POST" "boxes" '{
   "reference": "2",
   "size": "120x120x5",
   "location": "E20-F06",
-  "itemReference": "1",
+  "itemReference": "2",
   "numItems": 50
 }'
 
+
 send_request "POST" "orders" '{
-  "reference": "3",
-  "date": "17-11-2023",
+  "reference": "1",
+  "date": "29-12-2023",
   "state": "PENDING",
   "items": [
     {
-      "itemReference": "4",
-      "numberItems": 40
-    }
+      "itemReference": "1",
+      "numberItems": 10
+    },
+    {
+      "itemReference": "2",
+      "numberItems": 20
+    },
+    
   ]
 }'
 
